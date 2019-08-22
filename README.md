a5213dc870625832e71592f74583d0fd15d7b023
Vlad <vlos@nodesagency.com>
2019-08-22 15:01:25 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# docs_viewer
+
+A Flutter project which displays basic contents of a Google Docs document.
+
+Detailed article which explains the project is available at https://engineering.nodesagency.com/categories/android/2019/08/21/flutter-google-docs
+
+![Screenshot](screenshot.png)
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\dev\flutter\sdks\flutter-sdk"
+export "FLUTTER_APPLICATION_PATH=C:\dev\flutter\projects\events"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\dev\flutter\sdks\flutter-sdk\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
